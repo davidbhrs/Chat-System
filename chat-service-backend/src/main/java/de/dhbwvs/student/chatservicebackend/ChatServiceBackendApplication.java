d95e1ef0cb00bd2f0bea931311c5285dfa53bf3e
JulianGommlich <julian.gommlich@gmx.de>
2021-03-02 21:37:38 +0100

    Initialize Backend Project

---
+++ chat-service-backend/src/main/java/de/dhbwvs/student/chatservicebackend/ChatServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwvs.student.chatservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceBackendApplication.class, args);
+	}
+
+}
---
+++ chat-service-backend/src/test/java/de/dhbwvs/student/chatservicebackend/ChatServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwvs.student.chatservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
