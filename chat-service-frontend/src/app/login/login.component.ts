65769a98c383c34feaa896d3706c3c204db58e70
davidbhrs <d.baehrens@web.de>
2021-03-14 21:41:44 +0100

    login

--- chat-service-frontend/src/app/login/login.component.ts
+++ chat-service-frontend/src/app/login/login.component.ts
@@ -13,11 +13,24 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Login Method
+   * Calls the Login Method of the api-endpoint. --> Creates new User in Backend 
+   * 
+   * 
+   * @param username 
+   */
   login(username: String) {
-    let response: any
+    let success: Boolean
     this.api.login(username).subscribe(data => {
-      response = data;
-      console.log(response);
+      // User created successfully
+      console.log(data)
+      success = true;
+    },
+    error => {
+      // User is not created successfully --> User needs to get feedback
+      success = false;
+      console.error("User existiert bereits.")
     });
   }
 
