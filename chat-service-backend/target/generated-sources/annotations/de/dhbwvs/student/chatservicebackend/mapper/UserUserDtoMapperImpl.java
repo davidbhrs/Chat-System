ad088cff854b6ec15a399f4fd65de5eef1ef9d70
JulianGommlich <julian.gommlich@gmx.de>
2021-03-20 08:05:09 +0100

    Implement Workaround

--- chat-service-backend/target/generated-sources/annotations/de/dhbwvs/student/chatservicebackend/mapper/UserUserDtoMapperImpl.java
+++ chat-service-backend/target/generated-sources/annotations/de/dhbwvs/student/chatservicebackend/mapper/UserUserDtoMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-17T11:00:32+0100",
+    date = "2021-03-20T08:03:45+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 public class UserUserDtoMapperImpl implements UserUserDtoMapper {
